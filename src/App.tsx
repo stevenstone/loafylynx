dd084d4f5c59793fe55b1622d74407e8d75d6599
stevenstone <steven.stone0@gmail.com>
2026-03-28 21:17:47 -0500

    initial commit
    home page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Liam from "./assets/liam.jpeg";
+
+function App() {
+  return (
+    <>
+      <img src={Liam} alt="a side profile of a sitting golden retriever, nose up, smiling" width="604" height="1304" />
+    </>
+  )
+}
+
+export default App
