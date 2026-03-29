d23cc320dcd4c342145d70293a8b976d5a543d0c
stevenstone <steven.stone0@gmail.com>
2026-03-28 22:23:12 -0500

    adding some variety

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import './App.css';
 import Liam from "./assets/liam.jpeg";
+import Liam2 from "./assets/liam2.jpeg";
+import Moose from "./assets/moose.jpeg";
+import Storm from "./assets/storm.jpeg";
+import Cats from "./assets/cats.jpeg";
+import { useMemo } from 'react';
 
 function App() {
+  const index = Math.floor(Math.random() * 5);
+  let img;
+  switch (index) {
+    case 0:
+      img = Liam;
+      break;
+    case 1:
+      img = Liam2;
+      break;
+    case 2:
+      img = Moose;
+      break;
+    case 3:
+      img = Storm;
+      break;
+    case 4:
+      img = Cats;
+      break;
+  }
   return (
     <>
-      <img src={Liam} alt="a side profile of a sitting golden retriever, nose up, smiling" width="604" height="1304" />
+      {/* <img src={Liam} alt="a side profile of a sitting golden retriever, nose up, smiling" width="604" height="1304" /> */}
+      <img src={img} />
     </>
   )
 }
